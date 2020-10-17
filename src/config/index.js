8d52a821d44c78eeaa5b99ba8d6a8570887f7d39
xiaoc <1030718185@qq.com>
2020-10-17 16:34:57 +0800

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'Management', //项目名称
+  name: '深笃 LIMS', //项目名称
   leftNavdefaultOpenAll: false, //是否默认展开所有侧导航自导航  true展开  false不展开(会展开当前路由所在的一级菜单)
   CollapseModel: true, //侧导航切换的模式 true为手风琴模式  默认false
   collapsedDefaultOpen: false, //默认是否展开侧导航 true展开  false收起
